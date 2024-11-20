0f97b34195a77f128c622d77be42e4af8a6af0df
rahim8050 <rahimranxx8050@gmail.com>
2024-11-20 19:35:08 +0000

    dbs

--- dbs/app_forms.py
+++ dbs/app_forms.py
@@ -5,6 +5,7 @@ from dbs.models import Customer
 GENDER_CHOICES = {"Male": "Male", "Female": "Female"}
 
 class CustomerForm(forms.ModelForm):
+    gender = forms.ChoiceField(choices=GENDER_CHOICES,widget=forms.RadioSelect)
     class Meta:
         model = Customer
         fields = ['first_name', 'last_name', 'email', 'dob', 'weight', 'gender']
